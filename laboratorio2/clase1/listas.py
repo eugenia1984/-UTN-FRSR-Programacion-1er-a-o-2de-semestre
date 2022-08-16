a687075999c55281d8a08e52b89ffd8394f747d8
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-08-16 19:53:11 -0300

    seguimos con listas

--- laboratorio2/clase1/listas.py
+++ laboratorio2/clase1/listas.py
@@ -9,3 +9,17 @@ print(nombres[1])
 print(nombres[-1])
 # Para acceder al penultimo accedemos con -2, lo recorremos a la inversa a la lista, de atras hacia adelante
 print(nombres[-2])
+# Para imprmir solo los elementos con indice 0 y 1 de la lista
+# Le doy un rango, el segundo numero del rango no lo incluye
+print(nombres[0:2])
+# Para ir del inicio de la lista hasta un indice, sin incluirlo
+print(nombres[ :3])
+# Desde el indice indicado hasta el fnal
+print(nombres[1: ])
+# Para modificar un valor de la lista
+nombres[2] = 'Liliana'
+nombres[1] = 'Natalia'
+print(nombres)
+# Iterar una lista
+for nombre in nombres:
+  print(nombre)
