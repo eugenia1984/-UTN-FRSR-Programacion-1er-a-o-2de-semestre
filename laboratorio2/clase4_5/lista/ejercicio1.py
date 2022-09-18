0f9358ff38a215038b06e653049bec3775c3ac65
Eugenia <costamariaeugenia1@gmail.com>
2022-09-18 16:26:38 -0300

    Update ejercicio1.py

--- laboratorio2/clase4_5/lista/ejercicio1.py
+++ laboratorio2/clase4_5/lista/ejercicio1.py
@@ -2,3 +2,18 @@
 # Llenar una lista con los numeros del 1 al 50# Luego mostrar la lista con el bucle for
 # Los elementos deben mostrarse de la sigueinte forma:
 # 1 - 2 - 3 - 4 - 5 - .. - 50
+
+lista = []
+iterador = 1
+
+while iterador <= 50:
+  lista.append()
+  iterador += 1
+  
+for elemento in lista:
+  print(elemento, end="-")
+  
+# Otro modo de hacer la primer parte
+lista = list(range(1, 51))
+# Ahora imprimo lista
+print(lista)
