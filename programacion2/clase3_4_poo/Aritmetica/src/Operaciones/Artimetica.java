0634052d3edbdaa43cd20ebc9d31bb6cca31b198
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-25 17:49:50 -0300

    creo le ejercicio de la clase aritmetica para la suma

---
+++ programacion2/clase3_4_poo/Aritmetica/src/Operaciones/Artimetica.java
@@ -0,0 +1,15 @@
+package Operaciones;
+/**
+ * @author Maria Eugenia Costa
+ */
+public class Artimetica {
+    // Atributos de la clase
+    int numero1;
+    int numero2;
+        
+    // Metodo
+    public void sumerNumeros() {
+        int resultado = numero1 + numero2;
+        System.out.println("Resultado: " + resultado);
+    }
+}
