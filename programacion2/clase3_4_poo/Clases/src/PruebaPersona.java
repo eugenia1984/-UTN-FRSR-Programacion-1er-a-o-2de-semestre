ac232d28d986507a3b052634ab30c7262323a4f9
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-25 17:06:54 -0300

    agrego nueva persona

--- programacion2/clase3_4_poo/Clases/src/PruebaPersona.java
+++ programacion2/clase3_4_poo/Clases/src/PruebaPersona.java
@@ -16,6 +16,7 @@ public class PruebaPersona {
         
         // instancio un nuevo objeto de la clase Persona
         Persona persona2 = new Persona();
+        persona2.obtenerInformacion(); // Me da todo null porque no le asigne valores a los atributos
         System.out.println("persona2: " + persona2); // veo el espacio en memoria que ocupa
         System.out.println("persona1: " + persona1);
     }
