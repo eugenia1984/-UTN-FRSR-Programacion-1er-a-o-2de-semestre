4f94dbcf2eb99f9fcd76c6a727d6e5e65efb9f54
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-28 18:16:27 -0300

    creo la clase persona con atributos privados

--- programacion2/clase3_4_5_6_poo/Encapsulamiento/src/encapsulamiento/Persona.java
+++ programacion2/clase3_4_5_6_poo/Encapsulamiento/src/encapsulamiento/Persona.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package encapsulamiento;
 
-/**
- *
- * @author juan
- */
 public class Persona {
+    // Atributos
+    private String nombre;
+    private Double sueldo;
+    private boolean eliminado;
+    
+    // Constructor
+    public Persona(String nombre, double sueldo, boolean eliminado) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.eliminado = eliminado;
+    }
+    
+    // getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
     
 }
