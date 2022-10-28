b4bf525179d60451e278e76aab201d18e0d67a2c
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-28 20:01:26 -0300

    agrego dos objetos de la clase Persona

--- programacion2/clase7_8/ContextoEstatico/src/domain/Persona.java
+++ programacion2/clase7_8/ContextoEstatico/src/domain/Persona.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package domain;
 
-/**
- *
- * @author juan
- */
+
 public class Persona {
+    // Properties
+    private int idPersona;
+    private static int contadorPersona;
+    private String nombre;
+    
+    // constructores
+    public Persona() {
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+        // incrementamos el contador por cada objeto nuevo
+        Persona.contadorPersona++;
+        // asignamos un nuevo valor a la variable idPersona
+        this.idPersona = Persona.contadorPersona;
+    }
+    // getters y setters
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public static int getContadorPersona() {
+        return contadorPersona;
+    }
+
+    public static void setContadorPersona(int contadorPersona) {
+        Persona.contadorPersona = contadorPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    // toString
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona = " + idPersona + ", nombre = " + 
+            nombre + '}';
+    }
+    
+    
     
 }
---
+++ programacion2/clase7_8/ContextoEstatico/src/domain/PersonaPrueba.java
@@ -0,0 +1,15 @@
+
+package domain;
+
+public class PersonaPrueba {
+
+    public static void main(String[] args) {
+        // insancio un objeto de la clase Persona
+        Persona persona1 = new Persona("Marta");
+        System.out.println("persona1 -> " + persona1);
+        // instancio otro objeto de la clase Persona
+        Persona persona2 = new Persona("Sandra");
+        System.out.println("persona2 -> " + persona2);
+    }
+    
+}
