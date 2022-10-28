b52e203d419afd0d62dee314392871ba626f98ae
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-10-28 17:34:58 -0300

    agrego parte de la clase 7

--- programacion2/clase3_4_5_6_poo/Aritmetica/src/Operaciones/Persona.java
+++ programacion2/clase3_4_5_6_poo/Aritmetica/src/Operaciones/Persona.java
@@ -7,9 +7,18 @@ public class Persona {
         
     Persona(String nombre, String apellido) { // Constructor
         //Llamada al constructor de la clase padre Object
-        // super(); 
+        super(); 
+        
+        Imprimir imprimir = new Imprimir();
+        // new Imprimir().imprimir(this)
         this.nombre = nombre;
         this.apellido = apellido;
         System.out.println("Objeto persona usando this: " + this);
+    }
+}
+
+class Imprimir {
+    public Imprimir() {
+        super(); // el constructor de la clase padre para reservar memoria
     }
 }
---
+++ programacion2/clase3_4_5_6_poo/Encapsulamiento/src/encapsulamiento/Encapsulamiento.java
@@ -0,0 +1,10 @@
+
+package encapsulamiento;
+
+public class Encapsulamiento {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
